ca2a058b881ba5a422135b3d562d7f5dafbc2f03
Mo Bitar <me@bitar.io>
2017-02-24 17:23:38 -0600

    local markdown

--- lib/collab/engine.rb
+++ lib/collab/engine.rb
@@ -2,7 +2,6 @@ module Collab
   class Engine < ::Rails::Engine
     isolate_namespace Collab
 
-    # config.assets.paths << File.expand_path("../../../vendor/assets/javascripts", __FILE__)
-    config.assets.precompile += %w( aes.js hmac-sha256.js chainpad.js codemirror.js codemirror.css TextPatcher )
+    config.assets.precompile += %w( aes.js hmac-sha256.js chainpad.js codemirror.js codemirror.css TextPatcher markdown )
   end
 end
