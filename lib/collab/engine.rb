ba1ccbf009197c6a43c318a00a20aee2be681d4d
Johnny A <5891646+johnny243@users.noreply.github.com>
2021-06-16 14:08:50 -0400

    fix: assets collab/* not found error (#13)
    * fix: add sass-rails dependency
    
    * fix: update Gemfile.lock
    
    * fix: remove halt_callback_chains_on_return_false
    
    * fix: precompile collab/application.css and collab/application.js
    
    * chore: increment gem version
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>

--- lib/collab/engine.rb
+++ lib/collab/engine.rb
@@ -2,6 +2,6 @@ module Collab
   class Engine < ::Rails::Engine
     isolate_namespace Collab
 
-    config.assets.precompile += %w( aes.js hmac-sha256.js chainpad.js codemirror.js codemirror.css TextPatcher markdown )
+    config.assets.precompile += %w( collab/application.css collab/application.js aes.js hmac-sha256.js chainpad.js codemirror.js codemirror.css TextPatcher markdown )
   end
 end
